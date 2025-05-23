79976ec20afea593b9ee04c185c16aea586e18b6
kaarelkattai <kaarel.kattai@gmail.com>
2025-05-23 21:06:14 +0300

    added guests list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './shared/components/header/header.component';
+import {GuestTableComponent} from './features/guest-table/guest-table.component';
+import {GuestService} from './core/services/guest.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, GuestTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GuestManagerCLI';
+  guests: any[] = [];
+
+  constructor(
+    private guestService: GuestService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadGuests()
+  }
+
+  loadGuests(): void {
+    this.guestService.getAllGuests().subscribe({
+      next: (data) => {
+        this.guests = data.guests;
+      },
+      error: (err) => {
+        console.error('Failed to load guests:', err);
+      }
+    });
+  }
 }
